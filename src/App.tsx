4ed6e543ae08e626e60da3ed604b0a0fb68f85bf
Ricardo <ricardo_cabista@hotmail.com>
2023-10-15 14:01:50 -0300

    feat: add logo and remove icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { api } from './services/api'
 import { Link } from 'react-router-dom'
 
 import { getCorrespondingImage } from './utils/getCorrespondingImage'
-import { getCorrespondingIcon } from './utils/getCorrespondingIcon'
-
-import { RxArrowTopRight as ArrowIcon } from 'react-icons/rx'
 
 function App() {
   const [category, setCategory] = useState({})
@@ -28,18 +25,23 @@ function App() {
   }, [category, isEmpty])
 
   return (
-    <main className='container m-auto  bg-gradient-to-br from-gray-50 to-gray-100 '>
-      <h1 className='text-5xl py-10 m-auto text-center w-1/2'>
-        Choose one of the categories below to view more information
-      </h1>
-      <section className=' grid md:grid-cols-3 sm:grid-cols-1 gap-4  p-4'>
+    <main className='container m-auto  bg-[#000]'>
+      <div className='md:w-1/3 sm:w-full  m-auto h-[400px] relative '>
+        <img
+          src='
+            https://i.pinimg.com/564x/fc/e7/0f/fce70f99809f656e2ef741107ce7d84f.jpg
+            '
+          alt='star wars logo'
+          className='w-full h-full bg-center bg-cover'
+        />
+      </div>
+      <section className='grid md:grid-cols-3 sm:grid-cols-1 gap-4  p-4'>
         {category &&
           Object.keys(category).map((category, i) => {
-            const Icon = getCorrespondingIcon(category)
             return (
               <div
                 key={i}
-                className='bg-white relative btn-active-from-hover flex flex-col items-center  sm:h-[50vh] h-[50vh] md:h-[60vh]  justify-end border rounded-xl'
+                className=' relative btn-active-from-hover flex flex-col items-center  sm:h-[50vh] h-[50vh] md:h-[60vh]  justify-end rounded-xl'
               >
                 <div className='bg-purple-500  w-full h-full  rounded-xl '>
                   <img
@@ -51,15 +53,10 @@ function App() {
                 <div className='btn bg-black h-full w-full absolute top-0 hidden  items-center  justify-center rounded-xl backdrop-blur-lg filter bg-opacity-10 transition-all '>
                   <Link
                     to={`/${category}`}
-                    className='bg-gradient-to-b  from-black rounded-lg to-[#121212] text-white py-2 px-4    flex items-center gap-4'
+                    className='bg-gradient-to-b  from-black rounded-lg to-[#121212] text-white py-4 px-8    flex items-center gap-4'
                   >
-                    GET MORE <ArrowIcon size={15} />
+                    {category.toUpperCase()} INFO
                   </Link>
-                </div>
-
-                <div className='description absolute bottom-4 left-4 flex gap-4 backdrop-blur-sm filter bg-opacity-20 items-center bg-black text-white  py-2 px-6  rounded-xl'>
-                  <div>{Icon && <Icon size={25} />}</div>
-                  <p className=' text-xl text-white font-bold '>{category}</p>
                 </div>
               </div>
             )
