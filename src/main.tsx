d959204a5def80fa42b0a186640cd1b9d63609d9
Ricardo <ricardo_cabista@hotmail.com>
2023-10-15 23:14:54 -0300

    feat: add details route

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import CategoryPage from './pages/category/index.tsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import './index.css'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import CategoryPage from './pages/category/index.tsx'
+import DetailsPage from './pages/details/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/:category' element={<CategoryPage />} />
+        <Route path='/:category/:id?' element={<DetailsPage />} />
       </Routes>
     </Router>
   </React.StrictMode>
