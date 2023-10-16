26121c8a20570b885d1774c058d9dd0f6e7268bb
Ricardo <ricardo_cabista@hotmail.com>
2023-10-15 23:17:58 -0300

    feat: add loading component and apply some adjustments

--- src/pages/category/Table.tsx
+++ src/pages/category/Table.tsx
@@ -4,6 +4,9 @@ import { api } from '../../services/api'
 
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import { BsArrowLeft } from 'react-icons/bs'
+
+import Loading from '../../loading'
 
 const Table = () => {
   const { category } = useParams()
@@ -19,14 +22,11 @@ const Table = () => {
       const {
         data: { results }
       } = await api.get(`${category}`)
-      console.log(results, 'dados')
       setCategories(results)
     } catch (error) {
       console.log(error, 'error')
     }
   }
-
-  const getDetails = async (isDetails, value) => {}
 
   useEffect(() => {
     if (isEmpty) {
@@ -38,17 +38,30 @@ const Table = () => {
   const keys = categori && categori.map((c) => Object.keys(c))[0]
 
   const result =
-    keys && keys.filter((item) => item !== 'created' && item !== 'edited')
+    keys &&
+    keys.filter(
+      (item) => item !== 'created' && item !== 'edited' && item !== 'homeworld'
+    )
 
   return (
     <div className=' p-6 relative h-screen bg-cover bg-center bg-no-repeat'>
-      <div className='p-6 flex items-center gap-4 bg-black backdrop-blur-lg bg-opacity-20 filter text-white mb-8 rounded-xl'>
-        <h2 className='text-3xl'>More info from Star Wars {category} </h2>
+      <div className=' py-10  flex items-center '>
+        <div
+          className='flex gap-4 items-center px-6 py-3 bg-gradient-to-b from-yellow-500 to-yellow-600 text-yellow-950 rounded-lg  font-medium cursor-pointer opacity-80 hover:opacity-100'
+          onClick={() => navigate('/')}
+        >
+          <BsArrowLeft />
+          <p>BACK</p>
+        </div>
+      </div>
+      <div className='p-6 flex items-center gap-4 border border-[#333] bg-black backdrop-blur-lg bg-opacity-20 filter text-white mb-8 rounded-xl'>
+        <h2 className='text-3xl font-extrabold'> {category?.toUpperCase()} </h2>
       </div>
       <div className='w-full overflow-x-auto rounded-lg scrollbar-w-2 z-30 '>
         <table className='mx-auto max-w-4xl w-full whitespace-nowrap rounded-lg bg-gradient-to-b from-gray-100 to-gray-200 divide-y divide-gray-300 overflow-hidden'>
           <thead className='bg-gray-50'>
             <tr>
+              {!result && <Loading />}
               {result?.map((key) => {
                 const isUrlName =
                   key === 'url'
@@ -68,10 +81,16 @@ const Table = () => {
                 return (
                   <tr className=''>
                     {Object.keys(object)
-                      .filter((item) => item !== 'created' && item !== 'edited')
+                      .filter(
+                        (item) =>
+                          item !== 'created' &&
+                          item !== 'edited' &&
+                          item !== 'homeworld'
+                      )
                       .map((c) => {
                         const checkValuesTable =
                           isURL.test(object[c]) && !Array.isArray(object[c])
+
                         return (
                           <td
                             className='py-3   bg-gradient-to-b from-gray-100 to-gray-200 px-6 text-left max-w-xs hover:bg-gray-100'
@@ -80,7 +99,7 @@ const Table = () => {
                             <p
                               className={`${
                                 checkValuesTable
-                                  ? 'cursor-pointer  bg-gradient-to-b from-yellow-400 to-yellow-500 text-yellow-950 rounded-lg p-2 text-xs '
+                                  ? 'cursor-pointer   bg-gradient-to-b from-yellow-400 to-yellow-500 text-yellow-950 rounded-lg p-2 text-xs '
                                   : 'truncate'
                               } `}
                               onClick={() =>
@@ -90,6 +109,7 @@ const Table = () => {
                               {Array.isArray(object[c]) && object[c].length}
                               {!Array.isArray(object[c]) &&
                                 !checkValuesTable &&
+                                c !== 'homeworld' &&
                                 object[c]}
                               {checkValuesTable && 'more details'}
                             </p>
